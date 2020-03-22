7dc95c3084f9d8de4d093dd3e2577fc4051395e3
unknown <s18911@pjwstk.edu.pl>
2020-03-22 17:01:42 +0100

    commit1

---
+++ cw3/Controllers/StudentsController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using cw3.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace cw3.Controllers{
+
+    [ApiController]
+    [Route("api/students")]
+
+    public class StudentsController : ControllerBase{
+        private IDbService dbService;
+
+        public StudentsController(IDbService dbService) {
+            this.dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudent(string orderBy) {
+            if (orderBy == "indexNumber") {
+                return Ok(dbService.GetStudents().OrderBy(idx => idx.indexNumber));
+            }
+            return Ok(dbService.GetStudents());
+        }
+        
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id) {
+            if (id == 1) {
+                return Ok("Bursztynowski");
+            } else if (id == 2) {
+                return Ok("Kowalski");
+            }
+            else{
+                return NotFound("Nie znaleziono studenta");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student){
+            student.indexNumber = $"s{new Random().Next(1,20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutStudent(int id) {
+            return Ok($"Aktualizacja studenta o id={id} dokończona");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id) {
+            return Ok($"Usuwanie studenta od id={id} ukonczona");
+        }
+    }
+}
---
+++ cw3/DAL/MockDbService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw3.DAL{
+    public class MockDbService : IDbService{
+        private static IEnumerable<Student> studentsList;
+
+        static MockDbService(){
+            studentsList = new List<Student>()
+            {
+                new Student{ idNumber = 1, firstName = "Michal",lastName = "Bursztynowski", indexNumber = "s1"},
+                new Student{ idNumber = 2, firstName = "Michal2",lastName = "Bursztynowski2", indexNumber = "s2"},
+                new Student{ idNumber = 3, firstName = "Michal3",lastName = "Bursztynowski3", indexNumber = "s3"}
+            };
+
+        }
+        public IEnumerable<Student> GetStudents(){
+            return studentsList;
+            throw new NotImplementedException();
+        }
+    }
+}
